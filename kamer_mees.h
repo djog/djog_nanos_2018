20551836371169af6da52945d87d6a2adffe18c0
richelbilderbeek <richel@richelbilderbeek.nl>
2019-03-01 11:06:12 +0100

    OCLint hoeft niet Q_OBJECT te checken

--- kamer_mees.h
+++ kamer_mees.h
@@ -11,7 +11,7 @@ class hoofd_scherm;
 
 class kamer_mees : public QWidget
 {
-  Q_OBJECT
+  Q_OBJECT //!OCLINT
 
 public:
   explicit kamer_mees(hoofd_scherm * het_hoofd_scherm, QWidget *parent = 0);
