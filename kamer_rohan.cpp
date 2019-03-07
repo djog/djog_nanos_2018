114c7008da52a60ec7aeaed46584e92be6aac636
rohangraaf <Rsw@techie.com>
2019-03-07 19:54:08 +0100

    Cool, dankjewel @richelbilderbeek

--- kamer_rohan.cpp
+++ kamer_rohan.cpp
@@ -21,11 +21,39 @@ kamer_rohan::~kamer_rohan()
   delete ui;
 }
 
-void kamer_rohan::on_commandLinkButton_clicked()
+
+
+
+void kamer_rohan::on_pushButton_6_clicked()
 {
     this->ui->knop_n->setVisible(true);
     this->ui->knop_w->setVisible(true);
     this->ui->knop_o->setVisible(true);
+    this->ui->knop1->setText("goed");
+}
+
+void kamer_rohan::on_pushButton_7_clicked()
+{
+  this->ui->knop1->setText("fout");
 }
 
 
+void kamer_rohan::on_pushButton_8_clicked()
+{
+   this->ui->knop1->setText("fout");
+}
+
+void kamer_rohan::on_knop_o_clicked()
+{
+ m_hoofd_scherm->ga_naar(kamer_soort::richel);
+}
+
+void kamer_rohan::on_knop_w_clicked()
+{
+     m_hoofd_scherm->ga_naar(kamer_soort::daan);
+}
+
+void kamer_rohan::on_knop_n_clicked()
+{
+     m_hoofd_scherm->ga_naar(kamer_soort::seny);
+}
