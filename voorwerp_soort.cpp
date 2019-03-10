1422b6c621dc43d2d628f91939151f643e97aa67
jesperdoekes <jesper.github@wjd.nu>
2019-03-10 10:28:37 +0100

    Makkelijker voorwerpen toevoegen

--- voorwerp_soort.cpp
+++ voorwerp_soort.cpp
@@ -1,9 +1,27 @@
 #include "voorwerp_soort.h"
 
 #include <cassert>
+
+struct voorwerp_t {
+    const voorwerp_soort soort;
+    const std::string woord;
+};
+
+static voorwerp_t voorwerpen[] = {
+    {voorwerp_soort::spiegel, "spiegel"},
+    {voorwerp_soort::pistool, "pistool"},
+    {voorwerp_soort::tak, "beschimmelde tak"},
+};
+
+static const int voorwerpen_len = sizeof(voorwerpen) / sizeof(voorwerp_t);
+
 std::string als_woord(const voorwerp_soort voorwerp)
 {
-  if (voorwerp == voorwerp_soort::spiegel) return "spiegel";
-  assert(!"Ik weet niet hoe ik een woord maak van dit voorwerp"); //!OCLINT deze manier is prima om aan te geven dat iets nooit gebeurt
-  return ""; // Dan return ik maar niks
+    for (int i = 0; i < voorwerpen_len; ++i) {
+        if (voorwerpen[i].soort == voorwerp) {
+            return voorwerpen[i].woord;
+        }
+    }
+    assert(!"Ik weet niet hoe ik een woord maak van dit voorwerp"); //!OCLINT deze manier is prima om aan te geven dat iets nooit gebeurt
+    return ""; // Dan return ik maar niks
 }
